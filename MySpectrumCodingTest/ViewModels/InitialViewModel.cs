f185a89ede02c4a5d4da327a0eacec39f21b3bed
Luis Hernandez <>
2020-02-27 11:30:01 -0800

    Updates

---
+++ MySpectrumCodingTest/ViewModels/InitialViewModel.cs
@@ -0,0 +1,14 @@
+﻿//using System;
+//using System.Collections.ObjectModel;
+//namespace MySpectrumCodingTest.ViewModels
+//{
+//	public class InitialViewModel : BaseViewModel
+//	{
+//		public InitialViewModel()
+//		{
+//			Modules = new ObservableCollection<ModuleItemViewModel>();
+//		}
+
+//		public ObservableCollection<ModuleItemViewModel> Modules { get; private set; }
+//	}
+//}
